54af1f29d7158fc47cb1a9bef89ed4ff7cbb2e0b
SuccessDavis <simondavid612@gmail.com>
2026-04-12 13:39:45 +0100

    fix: correct Spatie Activitylog v5 namespace (Traits -> Models/Concerns, LogOptions -> Support/LogOptions)

--- app/Models/AttendanceSetting.php
+++ app/Models/AttendanceSetting.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
 class AttendanceSetting extends Model
 {
